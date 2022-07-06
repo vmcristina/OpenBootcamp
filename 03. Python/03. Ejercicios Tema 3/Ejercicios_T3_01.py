b702f0fbc434e4dedb7707e9b7b4b59f83d4f7d7
Cristina <cristinavm21@hotmail.com>
2022-07-06 21:35:31 +0200

    Ejercicios_T3_01

---
+++ 03. Python/03. Ejercicios Tema 3/Ejercicios_T3_01.py
@@ -0,0 +1,21 @@
+
+nombre = 'Cristina'
+apellido = 'Villegas'
+edad = 18
+email = 'mail@mail.com'
+telefono = 666666666
+casado = False
+conHijos = False
+listaAmigos = ['Eva', 'Antonio', 'Lucas', 'Matilde']
+pelisVistas = {'peli1': 'El rey leon', 'peli2': 'Cars'}
+
+
+print (nombre)
+print (apellido)
+print(edad)
+print(email)
+print(telefono)
+print(casado)
+print(conHijos)
+print(listaAmigos)
+print(pelisVistas)

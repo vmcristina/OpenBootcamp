394c6308e0a3e05c795f960c33d8157963b3c7e6
Cristina <cristinavm21@hotmail.com>
2022-07-09 18:24:00 +0200

    Ejercicios_T4_01

---
+++ 03. Python/04. Ejercicios Tema 4/Ejercicios_T4_01.py
@@ -0,0 +1,8 @@
+#Escribe un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no
+
+edad = int (input ("¿Que edad tiene?: "))
+
+if edad >= 18:
+    print ("Usted es mayor de edad")
+else:
+    print ("Usted no es mayor de edad")

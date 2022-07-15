03e1bed78b0cc1c09613e386f1090e2e91630239
Cristina <cristinavm21@hotmail.com>
2022-07-15 11:52:23 +0200

    Ejercicio_T5_01

---
+++ 03. Python/05. Ejercicios Tema 5/Ejercicios_T5_01.py
@@ -0,0 +1,22 @@
+#Escribe una función que calcule el área de un triángulo, recibiendo la altura y la base como parámetros y 
+#otra función que calcule el área de un círculo recibiendo el radio del mismo
+
+
+
+
+def triangulo (base, altura):
+    area = (base * altura) / 2
+    print (area)
+
+triangulo (2, 4)
+
+
+def circulo (radio):
+    calculo_radio = 3.141592 * (radio**2)
+    print (calculo_radio)
+
+circulo (2)
+
+
+
+    

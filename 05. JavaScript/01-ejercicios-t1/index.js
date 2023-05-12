390b3a3f532b6b37044b75c93e93d2bba59deba2
vmcristina <cristinavm21@hotmail.com>
2023-05-12 13:56:23 +0200

    05_EjercicioT1

--- 05. JavaScript/01-ejercicios-t1/index.js
+++ 05. JavaScript/01-ejercicios-t1/index.js
@@ -1,1 +1,1 @@
-console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo")
+console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo.")
